cb68c642b9a155aa78bb591da4b207f968be1cee
Carla <carladheyslane2@gmail.com>
2023-10-14 16:39:22 -0300

    Estudos DIO sobre Stream API

---
+++ stream_api/src/functional_interface/SupplierExemplo.java
@@ -0,0 +1,21 @@
+//Replicado da aula sobre consumer da Camila Cavalcante
+package functional_interface;
+
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class SupplierExemplo {
+     public static void main(String[] args) {
+    // Usar o Supplier com expressão lambda para fornecer uma saudação personalizada
+    Supplier<String> saudacao = () -> "Olá, seja bem-vindo(a)!";
+
+    // Usar o Supplier para obter uma lista com 5 saudações
+    List<String> listaSaudacoes = Stream.generate(saudacao)
+        .limit(5)
+        .toList();
+
+    // Imprimir as saudações geradas
+    listaSaudacoes.forEach(System.out::println);
+  }
+}
